d99401874e3fe833fdc886590538cfe2d7b6c632
tolgakb <tolgakabaahmet@gmail.com>
2023-08-03 17:53:01 +0100

    User authentication functionalities such as login and logout are added and users are able to update their profile detail(username, email) and users are able to delete their account.

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 
 class CreateUserForm(UserCreationForm):
 
@@ -32,3 +33,30 @@ class CreateUserForm(UserCreationForm):
             raise forms.ValidationError('Your email is too long')
         
         return email
+    
+
+#Login Form
+
+class LoginForm(AuthenticationForm):
+
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+#Update Form
+
+class UpdateUserForm(forms.ModelForm):
+
+    password = None
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateUserForm, self).__init__(*args, **kwargs)
+
+        #mark email field as required
+        self.fields['email'].required = True
+    
+    class Meta:
+
+        model= User
+        fields = ['username', 'email']
+        exclude = ['password1', 'password2']
--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm, UpdateUserForm
 from django.contrib.auth.models import User
 
 from django.contrib.sites.shortcuts import get_current_site
@@ -9,6 +9,10 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -87,3 +91,78 @@ def email_verification_success(request):
 def email_verification_failed(request):
 
     return render(request, 'account/registration/email-verification-failed.html')
+
+
+def my_login(request):
+    
+    form = LoginForm()
+
+    if request.method == 'POST':
+
+        form = LoginForm(request, data = request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username = username, password = password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect('dashboard')
+            
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'account/my-login.html', context)
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect('store')
+
+
+@login_required(login_url = 'my-login')
+def dashboard(request):
+
+    return render(request, 'account/dashboard.html')
+
+@login_required(login_url = 'my-login')
+def profile_management(request):
+
+    #Updating user's username nad email
+
+    if request.method == 'POST':
+
+        user_form = UpdateUserForm(request.POST, instance = request.user)
+
+        if user_form.is_valid():
+
+            user_form.save()
+            return redirect('dashboard')
+    
+    user_form = UpdateUserForm(instance= request.user)
+
+    context = {
+        'user_form': user_form,
+    }
+    
+    return render(request, 'account/profile-management.html', context)
+
+@login_required(login_url = 'my-login')
+def delete_account(request):
+
+    user = User.objects.get(id = request.user.id)
+
+    if request.method == 'POST':
+
+        user.delete()
+        
+        return redirect('store')
+    
+    return render(request, 'account/delete-account.html')
