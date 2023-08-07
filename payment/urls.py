2889142c7d920efcec0966bb3e307060a9b09693
tolgakb <tolgakabaahmet@gmail.com>
2023-08-07 16:21:38 +0100

    Order completion function is added.

--- payment/urls.py
+++ payment/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
    path('payment-success', views.payment_success, name = 'payment-success'),
    path('payment-failed', views.payment_failed, name = 'payment-failed'),
    path('checkout', views.checkout, name= 'checkout'),
+   path('complete-order', views.complete_order, name= 'complete-order'),
+
 ]
